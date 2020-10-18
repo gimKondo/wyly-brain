499193ff882a94a6ab9ea6811eddfe9549916ac5
Kondo Daisuke <gim0kondo@gmail.com>
2020-10-19 02:07:29 +0900

    :green_heart: introduce unit test

---
+++ app/sample.py
@@ -0,0 +1,11 @@
+"""sample module
+"""
+
+
+def return_five():
+    """return 5(It's just sample)
+
+    Returns:
+        int: [description]
+    """
+    return 5
---
+++ tests/test_sample.py
@@ -0,0 +1,5 @@
+from app.sample import return_five
+
+
+def test_return_five():
+    assert return_five() == 5
